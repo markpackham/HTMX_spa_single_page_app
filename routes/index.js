d0cb0b5d1b6904bb9ee9f00637d8a48b59ec47c2
mark <markpackham1@gmail.com>
2024-08-06 11:59:55 +0100

    Boost all the links on the page
    https://youtu.be/xBYXfj3nhMU?si=-CoJEGJDGaZ8cPDM&t=112

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get("/", (req, res) => {
 
 router.get("/articles/:id", (req, res) => {
   const article = articles.find((a) => a.id === parseInt(req.params.id));
+  res.render("article", { title: article.name, article: article });
 });
 
 export default router;
