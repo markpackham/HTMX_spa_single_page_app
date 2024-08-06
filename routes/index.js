b578f83430a3ef7b8f601ada443fa30a3793007d
mark <markpackham1@gmail.com>
2024-08-06 13:59:45 +0100

    Add post for articles
    https://youtu.be/3Q0JmxKYzAA?si=_L0Zx-4pIgFnonH3&t=2

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,18 @@ router.get("/articles/:id", (req, res) => {
   res.render("article", { title: article.name, article: article });
 });
 
+router.post("/articles", (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 100000),
+    name,
+    body,
+  };
+
+  articles.push(article);
+
+  res.render("partials/list", { article: articles });
+});
+
 export default router;
